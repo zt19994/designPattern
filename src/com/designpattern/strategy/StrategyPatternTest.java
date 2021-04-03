3d2290f6ea131b3a4f1ed4e422b0a43b88834b23
zt19994 <zt191610942@163.com>
2021-04-03 17:47:35 +0800

    策略模式

---
+++ src/com/designpattern/strategy/StrategyPatternTest.java
@@ -0,0 +1,20 @@
+package com.designpattern.strategy;
+
+/**
+ * 策略模式测试类
+ *
+ * @author zt1994 2021/4/3 17:45
+ */
+public class StrategyPatternTest {
+
+    public static void main(String[] args) {
+        Context context = new Context(new OperationAdd());
+        System.out.println("10 + 5 = " + context.executeStrategy(10, 5));
+
+        context = new Context(new OperationSubtract());
+        System.out.println("10 - 5 = " + context.executeStrategy(10, 5));
+
+        context = new Context(new OperationMultiply());
+        System.out.println("10 * 5 = " + context.executeStrategy(10, 5));
+    }
+}
