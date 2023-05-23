85eee7e14cc95293e639000b5ecdd0f9b4df5f51
zt1994 <zt191610942@163.com>
2023-05-23 22:25:45 +0800

    享元模式

---
+++ src/com/designpattern/structurePattern/flyweight/Shape.java
@@ -0,0 +1,9 @@
+package com.designpattern.structurePattern.flyweight;
+
+/**
+ * @author zhongtao
+ * @date 2023/5/23 22:10
+ */
+public interface Shape {
+    void draw();
+}
---
+++ src/com/designpattern/structurePattern/flyweight/ShapeFactory.java
@@ -0,0 +1,22 @@
+package com.designpattern.structurePattern.flyweight;
+
+import java.util.HashMap;
+
+/**
+ * @author zhongtao
+ * @date 2023/5/23 22:10
+ */
+public class ShapeFactory {
+    private static final HashMap<String, Shape> circleMap = new HashMap<>();
+
+    public static Shape getCircle(String color) {
+        Circle circle = (Circle) circleMap.get(color);
+
+        if (circle == null) {
+            circle = new Circle(color);
+            circleMap.put(color, circle);
+            System.out.println("Creating circle of color : " + color);
+        }
+        return circle;
+    }
+}
